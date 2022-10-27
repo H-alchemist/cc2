4950400078f620dbafc01e92e0832f91fee5259b
echchabli_hamza <echchablihamza1@gmail.com>
2022-10-28 00:07:21 +0100

    cc2

---
+++ aa.java
@@ -0,0 +1,16 @@
+
+
+import java.util.ArrayList;
+
+public class aa {
+    public static void main(String[]a) {
+        ArrayList<Integer> Para=new ArrayList<>();
+        Para.add(1);
+        Para.add(1);
+        Para.add(1);
+        Para.add(1);
+        System.out.println(Para);
+        
+    }
+    ;
+}
